10f0c5c535dc87068ec7e1ac80b5c4e5beef8f22
Carlomadella <madella871@gmail.com>
2025-03-14 14:54:21 +0100

    02-strings: esercizio 2 completato

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -7,7 +7,10 @@ console.log(name)
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
-
+let number = 8;
+let text = "Messi"
+let bool = true
+console.log(number, text, bool)
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
 // in console la stringa con il nome completo
